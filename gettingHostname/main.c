55bc57e04544b2d56ff18a8ec25d5f535b0735bd
LazyHippogriff <vishal14101993@gmail.com>
2020-01-15 17:23:56 +0530

    Update main.c

--- gettingHostname/main.c
+++ gettingHostname/main.c
@@ -1,42 +1,45 @@
-#include <stdio.h> //for printf()
-#include <stdlib.h> //for exit()
 #include <arpa/inet.h> //for inet_pton()
 #include <netdb.h> // for NI_MAXHOST, getnameinfo() and gai_strerror()
-#include <errno.h> // for errno
-#include <string.h> // for strerror()
- 
-int main(int argc, char** argv) {
-  if(argc!=2) {
-    printf("\n%s [IP]\n",argv[0]);
-    printf("e.g. %s 10.32.129.77\n",argv[0]);
-    exit(-1);
+#include <stdio.h>
+#include <stdlib.h>
+
+union {
+  struct sockaddr         sa;
+  struct sockaddr_in      s4;
+  struct sockaddr_in6     s6;
+  struct sockaddr_storage ss;
+} addr;
+
+
+static int convert4(struct sockaddr_in *sa, const char *name)
+{
+  return inet_pton(sa->sin_family = AF_INET, name, &sa->sin_addr);
+}
+
+static int convert6(struct sockaddr_in6 *sa, const char *name)
+{
+  return inet_pton(sa->sin6_family = AF_INET6, name, &sa->sin6_addr);
+}
+
+
+int main(int argc, char** argv)
+{
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s [IP]\nE.g. %s 10.32.129.77\n", argv[0], argv[0]);
+    return 1;
   }
-  struct sockaddr_in sa;
-  int res = inet_pton(AF_INET, argv[1] , &sa.sin_addr);
-  switch(res) {
-    case 0: printf("\nInput address is not a valid IPv4 address.\n");
-    case -1: if(res == 1)
-               printf("\nError(%s)\n",strerror(errno));
-             int n_res = inet_pton(AF_INET6, argv[1] , &sa.sin_addr);
-             switch(n_res) {
-               case 0: printf("\nInput address is not a valid IPv6 address.\n");
-               case -1: if(n_res == 1)
-                          printf("\nError(%s)\n",strerror(errno));
-                        exit(-1);
-               case 1: sa.sin_family = AF_INET;
-             }
-             break;
-    case 1: sa.sin_family = AF_INET;
+
+  if (convert4(&addr.s4, argv[1]) != 1 && convert6(&addr.s6, argv[1]) != 1) {
+    fprintf(stderr, "%s: not a valid IP address.\n", argv[1]);
+    return 1;
   }
 
   char node[NI_MAXHOST];
-  res = getnameinfo((struct sockaddr*)&sa, sizeof(sa), node, sizeof(node), NULL, 0, 0);
-  if (res)
-  {
-    printf("%s\n", gai_strerror(res));
+  int res = getnameinfo(&addr.sa, sizeof addr, node, sizeof node, NULL, 0, NI_NAMEREQD);
+  if (res) {
+    fprintf(stderr, "%s: %s\n", argv[1], gai_strerror(res));
     exit(1);
   }
-  printf("\nIP[%s]\n",argv[1]);
-  printf("HOSTNAME[%s]\n", node);
-  return 0;
+
+  fprintf(stdout,"\nIP[%s],Hostname[%s]\n",argv[1],node);
 }
