946dbdcec0e9f44856cd81384c8082c10a463cff
LazyHippogriff <vishal14101993@gmail.com>
2020-09-25 18:14:05 +0530

    Update tokenBucket.hpp

--- tokenBucket/tokenBucket.hpp
+++ tokenBucket/tokenBucket.hpp
@@ -6,11 +6,12 @@
 #include <iostream>
 
 class tokenBucket{
-  
+
   public:
 
     /* Constructors */
-      tokenBucket():m_tokenFillRatePerSecond(0),m_bucketSize(0),m_availableTokens(0),m_lastRefillTime(0),m_timeIntervalPerRequestInMicroSeconds(0){}
+
+    tokenBucket():m_tokenFillRatePerSecond(0),m_bucketSize(0),m_availableTokens(0),m_lastRefillTime(0),m_timeIntervalPerRequestInMicroSeconds(0){}
 
     tokenBucket(const tokenBucket& src):m_tokenFillRatePerSecond(src.m_tokenFillRatePerSecond),m_bucketSize(src.m_bucketSize),m_availableTokens(src.m_availableTokens),m_lastRefillTime(src.m_lastRefillTime),m_timeIntervalPerRequestInMicroSeconds(src.m_timeIntervalPerRequestInMicroSeconds)  {}
 
@@ -22,26 +23,28 @@ class tokenBucket{
       m_timeIntervalPerRequestInMicroSeconds = src.m_timeIntervalPerRequestInMicroSeconds;
       return *this;
     }
-  
-    tokenBucket(uint64_t fa_tokenFillRatePerSec): m_tokenFillRatePerSecond(fa_tokenFillRatePerSec),m_bucketSize(fa_tokenFillRatePerSec),m_availableTokens(fa_tokenFillRatePerSec),m_lastRefillTime(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count()),m_timeIntervalPerRequestInMicroSeconds(NUMBER_OF_MICROSECONDS_IN_A_SECOND/fa_tokenFillRatePerSec){
-      fprintf(stdout,"\nToken Bucket initialised with the following values\nm_bucketSize(%lu)\nm_tokenFillRatePerSecond(%lu)\nm_availableTokens(%lu)\nm_lastRefillTime(%lu)\nm_timeIntervalPerRequestInMicroSeconds(%lu)\n",m_bucketSize,m_tokenFillRatePerSecond,m_availableTokens,m_lastRefillTime,m_timeIntervalPerRequestInMicroSeconds);
+
+    tokenBucket(uint64_t fa_tokenFillRatePerSec): m_tokenFillRatePerSecond(fa_tokenFillRatePerSec),m_bucketSize(fa_tokenFillRatePerSec),m_availableTokens(fa_tokenFillRatePerSec),m_lastRefillTime(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count()),m_timeIntervalPerRequestInMicroSeconds(double(NUMBER_OF_MICROSECONDS_IN_A_SECOND)/fa_tokenFillRatePerSec){
+      fprintf(stdout,"\nToken Bucket initialised with the following values\nm_bucketSize(%lu)\nm_tokenFillRatePerSecond(%lu)\nm_availableTokens(%lu)\nm_lastRefillTime(%lu)\nm_timeIntervalPerRequestInMicroSeconds(%f)\n",m_bucketSize,m_tokenFillRatePerSecond,m_availableTokens,m_lastRefillTime,m_timeIntervalPerRequestInMicroSeconds);
     }
 
-    /* always called to check if sufficient number of tokens are available to server the request*/
-    bool areTokensAvailable(int fa_numberOfRequestedTokens = 1){
+
+    uint64_t getTokens(uint64_t fa_numberOfRequestedTokens = 1){
       std::lock_guard<std::mutex> lg(m_mu);
       uint64_t now = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
       refill_tokens(now);
 
+      uint64_t tokensToBeReturned = 0;
       if(m_availableTokens < fa_numberOfRequestedTokens){
-        return false;
+        tokensToBeReturned = m_availableTokens;
+        m_availableTokens = 0;
       }
       else{
         m_availableTokens -= fa_numberOfRequestedTokens;
-        return true;
+        tokensToBeReturned = fa_numberOfRequestedTokens;
       }
+      return tokensToBeReturned;
     }
-
 
   private:
     static constexpr int NUMBER_OF_MICROSECONDS_IN_A_SECOND = 1000000;
@@ -50,14 +53,15 @@ class tokenBucket{
     uint64_t m_tokenFillRatePerSecond;
     uint64_t m_availableTokens; //current number of tokens available in the bucket
     uint64_t m_lastRefillTime;
-    uint64_t m_timeIntervalPerRequestInMicroSeconds; // = (pow(10,6)/m_tokenFillRatePerSecond)
+    double m_timeIntervalPerRequestInMicroSeconds; // = (pow(10,6)/m_tokenFillRatePerSecond)
 
     /* to refill the tokens as per time diff */
     void refill_tokens(const uint64_t& fa_currentRequestTime) {
-      /*calculate tokens added in particular time difference */
-      if (fa_currentRequestTime >= m_lastRefillTime + m_timeIntervalPerRequestInMicroSeconds) {
+      auto timeElapsed = fa_currentRequestTime - m_lastRefillTime;
 
-        uint64_t l_newTokens = (fa_currentRequestTime - m_lastRefillTime)/m_timeIntervalPerRequestInMicroSeconds;
+      if (timeElapsed >= m_timeIntervalPerRequestInMicroSeconds) {
+
+        uint64_t l_newTokens = timeElapsed/m_timeIntervalPerRequestInMicroSeconds;
 
         if(m_availableTokens + l_newTokens < m_bucketSize) {
           m_availableTokens += l_newTokens;
