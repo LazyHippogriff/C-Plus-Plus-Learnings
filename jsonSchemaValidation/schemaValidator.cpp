6bbcf60fffff2142693a8c8625fdcc5c02a05f68
LazyHippogriff <vishal14101993@gmail.com>
2020-10-21 13:47:08 +0530

    Update schemaValidator.cpp
    Program now accepts the schema in input arguments.

--- jsonSchemaValidation/schemaValidator.cpp
+++ jsonSchemaValidation/schemaValidator.cpp
@@ -1,6 +1,6 @@
 /**
- * This program will take the path of a json file in the input and check if it
- * conforms to the schema defined in this program(std::string g_schema).
+ * This program will take the path of a JSON schema and JSON file in the input and check if the JSON file
+ * conforms to the schema.
  */
 
 #include "rapidjson/error/en.h"
@@ -9,35 +9,19 @@
 #include <fstream>
 #include <string>
 
-const std::string g_schema = R"({
-"$schema": "http://json-schema.org/draft-06/schema#",
-"title": "sampleSchema",
-"description": "A sample schema containing IPv4/v6",
-"type": "object",
-"properites": {
-  "ipv4": {
-    "type": "string"
-  },
-  "ipv6": {
-    "type": "string"
-  }
-},
-"required": [
-  "ipv4",
-  "ipv6"
-]
-})";
-
 int main(int argc, char * argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s test.json\n", argv[0]);
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s schema.json inputJson.json\n", argv[0]);
     return EXIT_FAILURE;
   }
 
-  // Read JSON schema.
   rapidjson::Document l_jsonSchemaDoc, l_jsonDoc;
 
-  l_jsonSchemaDoc.Parse(g_schema.c_str());
+  // Read JSON schema.
+  std::ifstream l_jsonSchemaFile(argv[1]);
+  std::string l_jsonSchema((std::istreambuf_iterator < char > (l_jsonSchemaFile)), (std::istreambuf_iterator < char > ()));
+
+  l_jsonSchemaDoc.Parse(l_jsonSchema.c_str());
   if (l_jsonSchemaDoc.HasParseError()) {
     fprintf(stderr, "Schema is not a valid JSON\n");
     fprintf(stderr, "Error(offset %u): %s\n",
@@ -49,9 +33,9 @@ int main(int argc, char * argv[]) {
   // Then convert the Document into SchemaDocument
   rapidjson::SchemaDocument sd(l_jsonSchemaDoc);
   rapidjson::SchemaValidator validator(sd);
-  
+
   //Read JSON which has to be validated
-  std::ifstream l_jsonFile(argv[1]);
+  std::ifstream l_jsonFile(argv[2]);
   std::string l_json((std::istreambuf_iterator < char > (l_jsonFile)), (std::istreambuf_iterator < char > ()));
   l_jsonDoc.Parse(l_json.c_str());
   if (l_jsonDoc.HasParseError()) {
@@ -79,3 +63,4 @@ int main(int argc, char * argv[]) {
     return EXIT_SUCCESS;
   }
 }
+
