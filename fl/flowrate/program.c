76b83364a8c0d7c1b925fb086840daffe51cfe70
LazyHippogriff <vishal14101993@gmail.com>
2020-12-20 19:21:03 +0530

    Update program.c

--- fl/flowrate/program.c
+++ fl/flowrate/program.c
@@ -3,38 +3,82 @@
 #include <math.h>
 #include <string.h>
 
+
 FILE * outputFile = NULL;
 long width, depth, rise, run;
 float velocity, hydraulicRadius, bedSlope;
+char materialName[5][100];
+float manningRoughnessCoefficients[5];
+
 
 //Declaration of functions used in main()
-int getDataFromInputFile(char materialName[5][100],float manningRoughnessCoefficients[5]);
+int getDataFromInputFile();
+void getDataFromUser();
 float calculateHydraulicRadius(const long area, const long wettedPerimeter);
 float calculateVelocity(const float hydraulicRadius, const float bedSlope, const float manningRoughnessCoefficient);
 float calculateFlowRate(const float velocity, const long width, const long depth);
 void printProgramIntroduction();
-void printChannel(const long width, const long depth);
+void printChannel();
 int manipulateWidth(int width, int depth);
 int manipulateDepth(int width, int depth);
-int displayAndSaveOutput(const float slope, FILE* outputFile);
+int displayAndSaveOutput();
 
 
 int main() {
-  char materialName[5][100];
-  memset(materialName,0,500);
-  float manningRoughnessCoefficients[5]={0};
-  getDataFromInputFile(materialName,manningRoughnessCoefficients);
+
   printProgramIntroduction();
+
+  if(getDataFromInputFile() != 0) {
+    fprintf(stderr,"\nError while getting data from the input file. Terminating the application\n");
+    return -1;
+  }
+
+  getDataFromUser();
+
+  printChannel();
+
+  if(displayAndSaveOutput() != 0) {
+    fprintf(stderr,"\nError while displaying/saving the output. Terminating the application\n");
+    return -1;
+  }
+
+  return 0;
+}
+
+void getDataFromUser() {
   fprintf(stdout,"\nPlease enter the following channel properties:\n\tWidth (m) -> ");
   scanf("%ld", &width);
+  while(width < 0) {
+    fprintf(stdout,"\nPlease enter a positive value of width\n");
+    fprintf(stdout,"\nPlease enter width of the channel(m) -> ");
+    scanf("%ld", &width);
+  }
+
   fprintf(stdout,"\tDepth (m) -> ");
   scanf("%ld", &depth);
+  while(depth < 0) {
+    fprintf(stdout,"\nPlease enter a positive value of depth\n");
+    fprintf(stdout,"\nPlease enter depth of the channel(m) -> ");
+    scanf("%ld", &depth);
+  }
+
   fprintf(stdout,"Please enter the slope parameters:\n\tRise (m) -> ");
         scanf("%ld",&rise);
+  while(rise < 0) {
+    fprintf(stdout,"\nPlease enter a positive value of rise\n");
+    fprintf(stdout,"\nPlease enter rise of the channel(m) -> ");
+    scanf("%ld", &rise);
+  }
+
         fprintf(stdout,"\tRun (m) -> ");
-        scanf("%ld",&run);
-        printChannel(width,depth);
-        return 0;
+  scanf("%ld",&run);
+  while(run < 0) {
+    fprintf(stdout,"\nPlease enter a positive value of run\n");
+    fprintf(stdout,"\nPlease enter run of the channel(m) -> ");
+    scanf("%ld", &run);
+  }
+
+  bedSlope = rise/run;
 }
 
 
@@ -54,24 +98,29 @@ float calculateFlowRate(const float velocity, const long width, const long depth
         return flowRate;
 }
 
-int getDataFromInputFile(char materialName[5][100],float manningRoughnessCoefficients[5]) {
+int getDataFromInputFile() {
         FILE* inputFile = fopen("inputFile.txt","r");
         if(!inputFile) {
                 fprintf(stdout,"\nError in opening file(inputFile.txt). Please make sure that this file is available\n");
                 return -1;
         }
-    else {
-                        int index = 0;
-                        while (fscanf(inputFile, "%20[^,],%f\n", materialName[index],&manningRoughnessCoefficients[index]) == 2) {
-                                fprintf(stdout,"\nMaterial-->%s,ManningRoughnessCoefficient-->%f\n",materialName[index],manningRoughnessCoefficients[index]);
-                                ++index;
-                                if(index == 5) {
-                                        break;
-                                }
+        else {
+                int index = 0;
+                while (fscanf(inputFile, "%20[^,],%f\n", materialName[index],&manningRoughnessCoefficients[index]) == 2) {
+                        //fprintf(stdout,"\nMaterial-->%s,ManningRoughnessCoefficient-->%f\n",materialName[index],manningRoughnessCoefficients[index]);
+                        ++index;
+                        if(index == 5) {
+                                break;
                         }
-                        fclose(inputFile);
                 }
-  return 0;
+                if(index != 5) {
+                        fprintf(stdout,"\ninputFile.txt doesn't contain 5 lines. There should be five entries, one per line, in this file. Each line will consist of a name for the material and the associated roughness.\n");
+                        fclose(inputFile);
+                        return -1;
+                }
+                fclose(inputFile);
+        }
+        return 0;
 }
 
 void printProgramIntroduction() {
@@ -96,7 +145,7 @@ void printProgramIntroduction() {
   fprintf(stdout,"- Using Manning's equation calculate the flow rate of an open channel for a range of materials & bed slopes\n");
 }
 
-void printChannel(const long width, const long depth) {
+void printChannel() {
   fprintf(stdout,"\nThe channel\n***********\n");
   for(int row = 0;row < depth + 3; ++row) {
     fprintf(stdout,"\n\t*");
@@ -130,26 +179,34 @@ void printChannel(const long width, const long depth) {
 
 
 
-int displayAndSaveOutput(const float slope, FILE* outputFile) {
+int displayAndSaveOutput() {
+        outputFile = fopen("theAdventuresOfZorro.txt","w");
+        if(!outputFile) {
+                fprintf(stdout,"\nError in opening output file(theAdventuresOfZorro.txt).\n");
+                return -1;
+        }
+
         fprintf(stdout,"\n%-25s*\t\t\tSlope\n"," ");
-        fprintf(stdout,"\n%-15s%-10s*  %-7f  *  %-7f  *  %-7f  *  %-7f  *  %-7f  *  \n","Material"," ",slope,slope+0.05,slope+0.1,slope+0.15,slope+0.2);
+        fprintf(stdout,"\n%-15s%-10s*  %-7f  *  %-7f  *  %-7f  *  %-7f  *  %-7f  *  \n","Material"," ",bedSlope,bedSlope+0.05,bedSlope+0.1,bedSlope+0.15,bedSlope+0.2);
         for(int i=0;i<100;++i){
                 fprintf(stdout,"-");
         }
         fprintf(stdout,"\n");
         for(int index=0;index<5;index++) {
-                fprintf(stdout,"%-25s*  %-7f  |  %-7f  |  %-7f  |  %-7f  |  %-7f  |\n",materialName[index],mValue[index],mValue[index],mValue[index],mValue[index],mValue[index]);
+                fprintf(stdout,"%-25s*  %-7f  |  %-7f  |  %-7f  |  %-7f  |  %-7f  |\n",materialName[index],manningRoughnessCoefficients[index],manningRoughnessCoefficients[index],manningRoughnessCoefficients[index],manningRoughnessCoefficients[index],manningRoughnessCoefficients[index]);
         }
 
-        fprintf(out_file,"\n%-25s*\t\t\tSlope\n"," ");
-        fprintf(out_file,"\n%-15s%-10s*  %-7f  *  %-7f  *  %-7f  *  %-7f  *  %-7f  *  \n","Material"," ",0.01,0.06,0.11,0.16,0.21);
+        fprintf(outputFile,"\n%-25s*\t\t\tSlope\n"," ");
+        fprintf(outputFile,"\n%-15s%-10s*  %-7f  *  %-7f  *  %-7f  *  %-7f  *  %-7f  *  \n","Material"," ",0.01,0.06,0.11,0.16,0.21);
         for(int i=0;i<100;++i){
-                fprintf(out_file,"-");
+                fprintf(outputFile,"-");
         }
-        fprintf(out_file,"\n");
+        fprintf(outputFile,"\n");
         for(int index=0;index<5;index++) {
-                fprintf(out_file,"%-25s*  %-7f  |  %-7f  |  %-7f  |  %-7f  |  %-7f  |\n",materialName[index],mValue[index],mValue[index],mValue[index],mValue[index],mValue[index]);
+                fprintf(outputFile,"%-25s*  %-7f  |  %-7f  |  %-7f  |  %-7f  |  %-7f  |\n",materialName[index],manningRoughnessCoefficients[index],manningRoughnessCoefficients[index],manningRoughnessCoefficients[index],manningRoughnessCoefficients[index],manningRoughnessCoefficients[index]);
         }
 
-        fclose(out_file);
+        fclose(outputFile);
+        return 0;
 }
+
